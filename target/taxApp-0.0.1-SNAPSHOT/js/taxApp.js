6b3ccf5945e0c0468d3b0635076f12a187538311
cshoemak <phrdrakhon@gmail.com>
2014-10-02 16:55:58 -0700

    Removing target folder.

--- target/taxApp-0.0.1-SNAPSHOT/js/taxApp.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$('.datepick').each(function() {
-			$(this).attr("type", "text"); // change input type to text so chrome doesn't use its date picker.
-			$(this).datepicker(); // set up datepickers
-	});
-});
